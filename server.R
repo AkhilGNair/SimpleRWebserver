dd176cd8621388d90cccb1883bd5e7dec2dc1b39
Akhil Nair <akhil.nair@amey.co.uk>
2017-07-29 17:07:40 +0100

    Handle underscores

--- server.R
+++ server.R
@@ -17,7 +17,8 @@ while (TRUE) {
   data_split = strsplit(data, " ")[[1]]
 
   type  = data_split[1]
-  data  = gsub("[[:punct:]]", "", data_split[2])
+  data  = gsub("_", " ", data_split[2]) 
+  data  = gsub("[[:punct:]]", "", data)
   proto = data_split[3]
 
   print(paste('Routed to', data))
